276dbc11fe314ad48879078fb0671b368baf0010
Andrew Hutson <andrew.hutson@outlook.com>
2016-12-01 16:14:47 -0800

    Renamed Source.cpp to Main.cpp because reasons. Added initial code: essentially a glorified hello world app.

---
+++ CS273 Final Project/CS273 Final Project/Main.cpp
@@ -0,0 +1,21 @@
+/////////////////////////////////////////////////////////
+// Program Name: Emergency Room Simulator
+// Authors: Andrew Hutson and McKenna Galle
+// Copyright © 2016 All rights reserved.
+/////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+
+	return 0;
+}
