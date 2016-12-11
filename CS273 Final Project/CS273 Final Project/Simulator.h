6b90ee7182ab72b670ff24b5b657be6b8b87d23e
Andrew Hutson <andrew.hutson@outlook.com>
2016-12-11 00:08:24 -0800

    Added initial code for Simulator.h and tweaked some formatting with the UML.

--- CS273 Final Project/CS273 Final Project/Simulator.h
+++ CS273 Final Project/CS273 Final Project/Simulator.h
@@ -1,7 +1,33 @@
 #ifndef SIMULATOR_H_
 #define SIMULATOR_H_
 
+#include <iostream>
+#include <string>
 
+// I think we might need to add queues like the airport. Or we could maybe do it with the EmergencyRoom class. We should talk about that.
+// We'll have to add queues somehow; I'm just not sure how.
 
+class Simulator
+{
+private:
+	int clock;
+	int totalTime;
+
+public:
+	Simulator()
+	{
+
+	}
+
+	void runSimulator()
+	{
+
+	}
+
+	void showStats()
+	{
+
+	}
+};
 
 #endif
