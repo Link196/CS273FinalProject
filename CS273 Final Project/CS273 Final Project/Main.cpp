028b0653411c0f8e5ab25de3d46da182662c336e
Andrew Hutson <andrew.hutson@outlook.com>
2016-12-12 20:13:28 -0800

    Removed unncecessary "include" and "using" statements from main.cpp

--- CS273 Final Project/CS273 Final Project/Main.cpp
+++ CS273 Final Project/CS273 Final Project/Main.cpp
@@ -4,28 +4,12 @@
 // Copyright © 2016 All rights reserved.
 /////////////////////////////////////////////////////////
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include "Caregiver.h"
-#include "Doctor.h"
-// #include "EmergencyRoom.h" - probably going to get deleted
-// #include "EntranceQueue.h"
-#include "Nurse.h"
-#include "Patients.h"
 #include "Simulator.h"
-
-
-using std::cout;
-using std::endl;
-using std::cin;
-using std::string;
 
 Simulator Emergency;
 
 int main()
 {
-	
 	Emergency.Data();
 	Emergency.runSimulator();
 	Emergency.showStats();
